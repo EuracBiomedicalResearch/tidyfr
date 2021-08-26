613b57f4dedae12dc15022282cf79b832ae98204
jorainer <johannes.rainer@gmail.com>
2021-08-26 08:51:30 +0200

    refactor: modify validation methods to also return silently

--- R/chris-dataset.R
+++ R/chris-dataset.R
@@ -5,7 +5,7 @@
 #' @param x `character(1)` representing the path which contains the CHRIS
 #'     data files.
 #'
-#' @param silent `logical(1)` to enable *silent mode*. If `FALSE` (the default)
+#' @param stop `logical(1)` to enable *silent mode*. If `FALSE` (the default)
 #'     an error will cause a `stop` call. If `TRUE` a `logical(1)` is returned
 #'     whether the data set is valid or not.
 #' 
@@ -14,34 +14,38 @@
 #' @author Johannes Rainer
 #'
 #' @noRd
-.check_dataset_content <- function(x, silent = FALSE) {
+.check_dataset_content <- function(x, stop = TRUE) {
     fls <- dir(x)
     if (!all(basename(fls) %in% c("data.txt", "groups.txt", "grp_labels.txt",
                                   "info.txt", "labels.txt", "mapping.txt"))) {
-        if (silent) return(FALSE)
-        stop("Folder ", x, " is missing one or more required data files.")
+        if (stop)
+            stop("Folder ", x, " is missing one or more required data files.")
+        else return(FALSE)
     }
     ## Test content of various data files.
     data <- .data(x)
-    .valid_data(data)
-    .valid_info(.info(x))
+    msgs <- character()
+    msgs <- c(msgs, .valid_data(data, stop = stop))
+    msgs <- c(msgs, .valid_info(.info(x), stop = stop))
     labels <- .labels(x)
-    .valid_labels(labels)
+    msgs <- c(msgs, .valid_labels(labels, stop = stop))
     ## Check that all columns in data are also present in labels.
     miss <- which(!(colnames(data) %in% c("aid", labels$label)))
     if (length(miss))
-        stop("missing labels for data column(s) ",
-             paste0(colnames(data)[miss], collapse = ", "))
+        msgs <- c(msgs, paste0("missing labels for data column(s) ",
+                               paste0(colnames(data)[miss], collapse = ", ")))
     ## Check that we have mappings for all categorical variables.
     map <- .mapping(x)
     cats <- labels$label[labels$type == "categorical"]
     if (length(cats)) {
         miss <- which(!cats %in% map$label)
         if (length(miss))
-            stop("Missing encoding for categorical variables: ",
-                 paste0(cats[miss], collapse = ", "))
+            msgs <- c(msgs, "Missing encoding for categorical variables: ",
+                      paste0(cats[miss], collapse = ", "))
     }
-    invisible(TRUE)
+    if (stop && length(msgs))
+        stop(msgs)
+    !length(msgs)
 }
 
 .read_dataset_file <- function(x, name) {
@@ -53,89 +57,116 @@
     .read_dataset_file(x, "data")
 }
 
-.valid_data <- function(x) {
+.valid_data <- function(x, stop = TRUE) {
+    msgs <- character()
     if (!any(colnames(x) == "aid"))
-        stop("data.txt lacks required column 'aid'")
-    invisible(TRUE)
+        msgs <- c(msgs, "data.txt lacks required column 'aid'")
+    if (stop && length(msgs))
+        stop(msgs)
+    msgs
 }
 
 .groups <- function(x) {
     .read_dataset_file(x, "groups")
 }
 
-.valid_groups <- function(x) {
+.valid_groups <- function(x, stop = TRUE) {
+    msgs <- character()
     if (ncol(x) != 2)
-        stop("groups.txt is expected to have two columns.")
-    if (!all(colnames(x) == c("group", "label")))
-        stop("groups.txt lacks one or more required columns.")
-    invisible(TRUE)
+        msgs <- c(msgs, "groups.txt is expected to have two columns.")
+    if (!all(colnames(x) %in% c("group", "label")))
+        msgs <- c(msgs, "groups.txt lacks one or more required columns.")
+    if (stop && length(msgs))
+        stop(msgs)
+    msgs
 }
 
 .grp_labels <- function(x) {
     .read_dataset_file(x, "grp_labels")
 }
 
-.valid_grp_labels <- function(x) {
+.valid_grp_labels <- function(x, stop = TRUE) {
+    msgs <- character()
     if (ncol(x) != 2)
-        stop("grp_labels.txt is expected to have two columns.")
-    if (!all(colnames(x) == c("group", "description")))
-        stop("grp_labels.txt lacks one or more required columns.")
-    invisible(TRUE)
+        msgs <- c(msgs, "grp_labels.txt is expected to have two columns.")
+    if (!all(colnames(x) %in% c("group", "description")))
+        msgs <- c(msgs, "grp_labels.txt lacks one or more required columns.")
+    if (stop && length(msgs))
+        stop(msgs)
+    msgs
 }
 
 .info <- function(x) {
     .read_dataset_file(x, "info")
 }
 
-.valid_info <- function(x) {
+.valid_info <- function(x, stop = TRUE) {
+    msgs <- character()
     if (ncol(x) != 2)
-        stop("info.txt is expected to have two columns")
-    if (!all(colnames(x) == c("key", "description")))
-        stop("columns of info.txt are expected to be called 'key' ",
-             "and 'description'")
-    if (!all(c("name", "description", "version", "date") %in% x[, "key"]))
-        stop("info.txt is missing one or more required key entries")
-    invisible(TRUE)
+        msgs <- c(msgs, "info.txt is expected to have two columns")
+    if (!all(colnames(x) %in% c("key", "description")))
+        msgs <- c(msgs,
+                  paste0("columns of info.txt are expected to be called 'key' ",
+                         "and 'description'"))
+    else if (!all(c("name", "description", "version", "date") %in% x[, "key"]))
+        msgs <- c(msgs, "info.txt is missing one or more required key entries")
+    if (stop && length(msgs))
+        stop(msgs)
+    msgs
 }
 
 .labels <- function(x) {
     .read_dataset_file(x, "labels")
 }
 
-.valid_labels <- function(x) {
+.valid_labels <- function(x, stop = TRUE) {
+    msgs <- character()
     if (!all(c("label", "type", "min", "max", "missing", "description") %in%
              colnames(x)))
-        stop("labels.txt lacks one or more required columns.")
-    if (any(is.na(x[, "label"])) | length(which(x[, "label"] == "")))
-        stop("labels.txt: column 'label' contains missing or empty data")
-    if (any(!x[, "type"] %in% c("label", "character", "integer", "categorical",
-                                "float", "date", "time", "datetime",
-                                "boolean")))
-        stop("labels.txt: column 'type' contains unsupported values.")
-    invisible(TRUE)
+        msgs <- c(msgs, "labels.txt lacks one or more required columns.")
+    else {
+        if (any(is.na(x[, "label"])) | length(which(x[, "label"] == "")))
+            msgs <- c(msgs, paste0("labels.txt: column 'label' contains",
+                                   " missing or empty data"))
+        if (any(!x[, "type"] %in% c("label", "character", "integer",
+                                    "categorical", "float", "date", "time",
+                                    "datetime", "boolean")))
+            msgs <- c(
+                msgs, "labels.txt: column 'type' contains unsupported values.")
+    }
+    if (stop && length(msgs))
+        stop(msgs)
+    msgs
 }
 
 .mapping <- function(x) {
     .read_dataset_file(x, "mapping")
 }
 
-.valid_mapping <- function(x) {
+.valid_mapping <- function(x, stop = TRUE) {
+    msgs <- character()
     if (!all(c("label", "code", "value") %in% colnames(x)))
-        stop("mapping.txt misses one or more required columns")
-    invisible(TRUE)
+        msgs <- c(msgs, "mapping.txt misses one or more required columns")
+    if (stop && length(msgs))
+        stop(msgs)
+    msgs
 }
 
 #' Check that we have for all categorical variables the correct encodings.
 #' 
 #' @noRd
-.valid_mapping_category_codes <- function(data, mapping) {
+.valid_mapping_category_codes <- function(data, mapping, stop = TRUE) {
+    msgs <- character()
     maps <- split(mapping$code, map$label)
     for (variable in names(maps)) {
         uvals <- unique(data[, variable])
         if (!all(uvals) %in% maps[[variable]])
-            stop("Categorical variable '", variable, "' has values without ",
-                 "encoding.", call. = FALSE)
+            msgs <- c(msgs, paste0("Categorical variable '", variable,
+                                   "' has values without encoding."))
     }
+    if (stop && length(msgs))
+        stop(msgs)
+    msgs
 }
 
 #' Function to read the data, and format the columns correctly.
--- tests/testthat/test_chris-dataset.R
+++ tests/testthat/test_chris-dataset.R
@@ -5,69 +5,81 @@ test_that(".check_dataset_content works", {
     expect_true(.check_dataset_content(dr))
 
     dr <- system.file("txt", package = "chrisr")
-    expect_error(.check_dataset_content(dr), "missing one")
-    expect_false(.check_dataset_content(dr, TRUE))
+    expect_error(.check_dataset_content(dr, stop = TRUE), "missing one")
+    expect_false(.check_dataset_content(dr, stop = FALSE))
 })
 
 test_that(".data, .valid_data works", {
     data <- .data(ex1)
-    expect_true(.valid_data(data))
+    expect_true(length(.valid_data(data)) == 0)
     data$aid <- NULL
     expect_error(.valid_data(data), "lacks required")
+    expect_true(length(.valid_data(data, stop = FALSE)) == 1)
 })
 
 test_that(".groups, .valid_groups works", {
     groups <- .groups(ex1)
-    expect_true(.valid_groups(groups))
+    expect_true(length(.valid_groups(groups)) == 0)
     groups$other <- "d"
     expect_error(.valid_groups(groups), "two columns")
+    expect_true(length(.valid_groups(groups, stop = FALSE)) > 0)
     groups$other <- NULL
     colnames(groups) <- c("a", "b")
     expect_error(.valid_groups(groups), "required columns")
+    expect_true(length(.valid_groups(groups, stop = FALSE)) > 0)
 })
 
 test_that(".grp_labels, .valid_grp_labels works", {
     grpl <- .grp_labels(ex1)
-    expect_true(.valid_grp_labels(grpl))
+    expect_true(length(.valid_grp_labels(grpl)) == 0)
     grpl$other <- "d"
     expect_error(.valid_grp_labels(grpl), "two columns")
+    expect_true(length(.valid_grp_labels(grpl, stop = FALSE)) > 0)
     grpl$other <- NULL
     colnames(grpl) <- c("a", "b")
     expect_error(.valid_grp_labels(grpl), "required columns")
+    expect_true(length(.valid_grp_labels(grpl, stop = FALSE)) > 0)
 })
 
 test_that(".info, .valid_info works", {
     info <- .info(ex1)
     expect_true(ncol(info) == 2)
-    expect_true(.valid_info(info))
+    expect_true(length(.valid_info(info)) == 0)
     info$other <- "b"
     expect_error(.valid_info(info), "two columns")
+    expect_true(length(.valid_info(info, stop = FALSE)) > 0)
     info$other <- NULL
     colnames(info) <- c("a", "b")
     expect_error(.valid_info(info), "to be called")
+    expect_true(length(.valid_info(info, stop = FALSE)) > 0)
     colnames(info) <- c("key", "description")
     info <- info[1:3, ]
     expect_error(.valid_info(info), "required key")
+    expect_true(length(.valid_info(info, stop = FALSE)) > 0)
 })
 
 test_that(".labels, .valid_labels works", {
     l <- .labels(ex1)
-    expect_true(.valid_labels(l))
+    expect_true(length(.valid_labels(l)) == 0)
     l$type <- NULL
     expect_error(.valid_labels(l), "required columns")
+    expect_true(length(.valid_labels(l, stop = FALSE)) > 0)
     l <- .labels(ex1)
     l[2, "label"] <- NA
     expect_error(.valid_labels(l), "missing or empty")
+    expect_true(length(.valid_labels(l, stop = FALSE)) > 0)
     l[2, "label"] <- ""
     expect_error(.valid_labels(l), "missing or empty")
+    expect_true(length(.valid_labels(l, stop = FALSE)) > 0)
     l <- .labels(ex1)
     l[3, "type"] <- "unknown"
     expect_error(.valid_labels(l), "unsupported")
+    expect_true(length(.valid_labels(l, stop = FALSE)) > 0)
 })
 
 test_that(".mapping, .valid_mapping works", {
     m <- .mapping(ex1)
-    expect_true(.valid_mapping(m))
+    expect_true(length(.valid_mapping(m)) == 0)
     m$value <- NULL
     expect_error(.valid_mapping(m), "required columns")
 })
